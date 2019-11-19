a29efc17b672dcd0fbdf02fa0bf6d93bf61c69ce
AmiyaSatyam <amiyasatyam@gmail.com>
2019-11-20 04:01:41 +0530

    AddDataVariables

--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -6,5 +6,7 @@ namespace TrackerLibrary
 {
     public class TeamModel
     {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
     }
 }
